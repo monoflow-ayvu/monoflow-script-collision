c11244051ff7f6ee9fd761aaab0c94a6d0bc5bbc
Fernando Mumbach <fermuch@ayvu.net>
2022-03-12 17:26:00 -0300

    adding config and basic structure for script

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,44 @@
 import * as MonoUtils from "@fermuch/monoutils";
 
 // based on settingsSchema @ package.json
-type Config = Record<string, unknown> & {
-  nome: string;
+type Config = {
+  maxSamples: number;
+  minTimeBetweenSamplesMs: number;
+  visibleTimeRangeMs: number;
+  magnitudeThreshold: number;
+  percentOverThresholdForShake: number;
+  lockOnCollision: boolean;
 }
 
 const conf = new MonoUtils.config.Config<Config>();
+const IS_DEVICE_LOCKED_KEY = 'IS_DEVICE_LOCKED' as const;
+
+declare class ShakeEvent extends MonoUtils.wk.event.BaseEvent {
+  kind: 'shake-event';
+  getData(): {percentOverThreshold: number};
+}
 
 messages.on('onInit', function() {
-  platform.log('script started! (I am the script)');
+  platform.log('collision script started');
   platform.log('settings:');
   platform.log(conf.store);
 
-  const name = conf.get('nome', 'default name');
-  platform.log(`Hello, ${name}!`);
+  env.setData('ACCELEROMETER_MAX_SAMPLES', conf.get('maxSamples', 25));
+  env.setData('ACCELEROMETER_MIN_TIME_BETWEEN_SAMPLES_MS', conf.get('minTimeBetweenSamplesMs', 20));
+  env.setData('ACCELEROMETER_VISIBLE_TIME_RANGE_MS', conf.get('visibleTimeRangeMs', 500));
+  env.setData('ACCELEROMETER_MAGNITUDE_THRESHOLD', conf.get('magnitudeThreshold', 25));
+  env.setData('ACCELEROMETER_PERCENT_OVER_THRESHOLD_FOR_SHAKE', conf.get('percentOverThresholdForShake', 66));
+
+  MonoUtils.wk.event.subscribe<ShakeEvent>('shake-event', (ev) => {
+    platform.log(`detected shake event of magnitude ${ev.getData()?.percentOverThreshold}%`);
+    env.project?.saveEvent(ev);
+
+    if (conf.get('lockOnCollision', false)) {
+      platform.log('locking device');
+      MonoUtils.storage.set(IS_DEVICE_LOCKED_KEY, true);
+      MonoUtils.wk.lock.lock();
+      env.project.logout();
+    }
+  });
 });
+
